b63a3eeb08d005951bc8c9096238865d403d3fb8
David Wytenburg <david.wytenburg@gmail.com>
2015-12-20 17:15:45 -0500

    appfinished

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
-get '/' do
+require 'sinatra'
+require 'sinatra/reloader'
+require 'gravatar-ultimate'
 
+get '/' do
+	erb :form
 end
 
 post '/submitted' do
-
+	email = params[:email]
+	thing = Gravatar.new(params[:email]).image_url
+	erb :thanks, locals: {gravatar:thing, email:email}
 end
